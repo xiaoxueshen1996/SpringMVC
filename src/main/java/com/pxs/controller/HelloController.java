1736f85765fa6c53ba46373ac67840c818941eb8
xiaoxueshen1996 <996229297@qq.com>
2019-06-18 16:50:05 +0800

    springmvc day01

---
+++ src/main/java/com/pxs/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.pxs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        System.out.println("hello");
+        return "success";
+    }
+}
